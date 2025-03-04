454fafc153c03f17352855cf52a1859b08a59a41
Gazi Monir-Uz-Zaman Apu <ciphercall@gmail.com>
2025-03-04 09:25:51 +0600

    auth middleware added to livekit routes

--- routes/livekitRoutes.js
+++ routes/livekitRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const livekitController = require('../controllers/livekitController');
+const authenticateToken = require('../middlewares/auth');
 
 // Endpoint to generate a LiveKit token
 // Example: GET /livekit/token?room=demo-room&identity=anonymous
-router.get('/token', livekitController.getToken);
+router.get('/token', authenticateToken, livekitController.getToken);
 
 module.exports = router;
